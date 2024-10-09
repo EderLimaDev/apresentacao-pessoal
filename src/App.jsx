260fb4d837c3fa21e2d2648207ad267b249ff291
EderLimaDev <ederlimarh@gmail.com>
2024-10-09 18:58:55 -0300

    chore: initial folder, color and fonts setup

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ function App() {
   return (
     <>
       <main>
-        <h1 className="text-3xl font-bold underline">Seja bem vindo! :)</h1>
-        <p>Desafio de apresentação Pessoal do Projeto Frontend fusion</p>
+        <h1 className="text-5xl font-bold main-titles-font">SEJA BEM VINDO</h1>
+        <p className="flex bg-green-tone-700">Desafio de apresentação Pessoal do Projeto Frontend fusion</p>
       </main>
     </>
   );
