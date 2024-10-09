609c0b5cf999b3df9819f895b9650a710d31ccf6
EderLimaDev <ederlimarh@gmail.com>
2024-10-09 01:21:49 -0300

    chore: install TailwindCSS

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <main>
-        <h1>Seja bem vindo! :)</h1>
+        <h1 className="text-3xl font-bold underline">Seja bem vindo! :)</h1>
         <p>Desafio de apresentação Pessoal do Projeto Frontend fusion</p>
       </main>
     </>
