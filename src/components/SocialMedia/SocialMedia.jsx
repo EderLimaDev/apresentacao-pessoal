a87b002cd27c662b5cfdcc2c30930520800fbb29
EderLimaDev <ederlimarh@gmail.com>
2024-10-17 18:59:30 -0300

    chore: add links to social media

--- src/components/SocialMedia/SocialMedia.jsx
+++ src/components/SocialMedia/SocialMedia.jsx
@@ -2,21 +2,51 @@ import React from "react";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import InstagramIcon from "@mui/icons-material/Instagram";
-import { Box } from "@mui/material";
+import { Box, Link } from "@mui/material";
 
 export const SocialMedia = () => {
   return (
     <Box className="absolute right-3 top-0 h-full flex items-center z-30">
       <Box className="flex flex-col gap-6">
-        <GitHubIcon 
-          sx={{ fontSize: 40, cursor: "pointer", "&:hover": { fill: "#25f62f" } }}
-        />
-        <LinkedInIcon
-          sx={{ fontSize: 40, cursor: "pointer", "&:hover": { fill: "#25f62f" } }}
-        />
-        <InstagramIcon
-          sx={{ fontSize: 40, cursor: "pointer", "&:hover": { fill: "#25f62f" } }}
-        />
+        <Link
+          href="https://github.com/EderLimaDev/"
+          target="_blank"
+          color="inherit"
+        >
+          <GitHubIcon
+            sx={{
+              fontSize: 40,
+              cursor: "pointer",
+              "&:hover": { fill: "#25f62f" },
+            }}
+          />
+        </Link>
+        <Link
+          href="https://www.linkedin.com/in/ederlimadev/"
+          target="_blank"
+          color="inherit"
+        >
+          <LinkedInIcon
+            sx={{
+              fontSize: 40,
+              cursor: "pointer",
+              "&:hover": { fill: "#25f62f" },
+            }}
+          />
+        </Link>
+        <Link
+          href="https://www.instagram.com/ederlimapm/"
+          target="_blank"
+          color="inherit"
+        >
+          <InstagramIcon
+            sx={{
+              fontSize: 40,
+              cursor: "pointer",
+              "&:hover": { fill: "#25f62f" },
+            }}
+          />
+        </Link>
       </Box>
     </Box>
   );
