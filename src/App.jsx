514b66977a0dfebb1eb62a7c6fd346bcd8db6bf0
EderLimaDev <ederlimarh@gmail.com>
2024-10-17 18:39:10 -0300

    feat: add react router dom to manage routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 
-import { Footer } from "./components/Footer/Footer";
+import { Route, Routes } from "react-router-dom";
 import { Header } from "./components/Header/Header";
+import { Footer } from "./components/Footer/Footer";
 import { Home } from "./pages/Home/Home";
+import { Projects } from "./pages/Home/sections/Projects/Projects";
+import { About } from "./pages/Home/sections/About/About";
 
 function App() {
 
   return (
     <>
       <Header />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="projects" element={<Projects />} />
+        <Route path="about" element={<About />} />
+      </Routes>
       <Footer />
     </>
   );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const Header = () => {
   return (
     <nav className="fixed w-full z-20 text-green-tone-50 border-b-green-tone-800 border-b-2 h-14 top-0 centered-containers justify-evenly text-2xl bg-green-tone-950 opacity-2">
-      <div className="cursor-pointer hover:underline">Projetos</div>
-      <div className="cursor-pointer hover:underline">Contato</div>
-      <div className="cursor-pointer hover:underline">Sobre</div>
+      <Link to="/"><div className="cursor-pointer hover:underline">Home</div></Link>
+      <Link to="/projects"><div className="cursor-pointer hover:underline">Projetos</div></Link>
+      <Link to="/about"><div className="cursor-pointer hover:underline">Sobre</div></Link>
     </nav>
   );
 };
